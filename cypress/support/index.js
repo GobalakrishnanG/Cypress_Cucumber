562b1cb768d50a607add1b633cf6efeed8c3ef00
GobalakrishnanG <gobalakrishnan07@gmail.com>
2021-11-14 18:58:44 +0000

    sample commit

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -8,6 +8,8 @@
 // You can change the location of this file or turn off
 // automatically serving support files with the
 // 'supportFile' configuration option.
+
+// this is test message
 //
 // You can read more here:
 // https://on.cypress.io/configuration
